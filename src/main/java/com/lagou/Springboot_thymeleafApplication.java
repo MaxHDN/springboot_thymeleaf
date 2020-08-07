cd2485729d82d1bb6e9dc70acfaabc7dc42c56ae
MaxHDN <414283143@qq.com>
2020-08-07 09:39:08 +0800

    first commit

---
+++ src/main/java/com/lagou/Springboot_thymeleafApplication.java
@@ -0,0 +1,13 @@
+package com.lagou;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot_thymeleafApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Springboot_thymeleafApplication.class, args);
+    }
+
+}
